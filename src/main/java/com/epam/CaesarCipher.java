6551bba27ca380c5d54edc2c719397cb4f6d667d
Soroshkin Anton <soroshkin_a_m@mail.ru>
2020-01-14 10:29:54 +0300

    Update CaesarCipher class

--- src/main/java/com/epam/CaesarCipher.java
+++ src/main/java/com/epam/CaesarCipher.java
@@ -2,33 +2,43 @@ package main.java.com.epam;
 
 public class CaesarCipher {
     public static void main(String[] args) {
-String text = "Еъёчхф Вхзёюлх, адздёиу ф ждэщхб, црбх еёдюэчъщъгюъв южаижжзчх, ждчъёнъгжзчдв. Ъы зёюивй жёхчгюв бюнс ж ъы вдгивъгзхбсгрв аёхкдв. Зъеъёс вгъ дмъчющгд, мзд гъюэцъьгджзс тздшд аёхкх фчбфъзжф жбъщжзчюъв гъждчъёнъгжзчх мъбдчъмъжадшд югщючющиивх. Ф юэимюб чхни южздёюу ю чгыж юэвъгъгюф, здмгъъ дзёхэюч еджздфггиу юэвъгмючджзс мъбдчъмъжаюк едёдадч. Ю зъв гъ въгъъ, еджбъщдчхбх гъищхмх. Ф еёюнъб а чрчдщи, мзд чюгдя чжъви вдя югзъббъаз, х чдэвдьгд, вды мёъэвъёгдъ жзёъвбъгюъ ад чжъви шхёвдгюмгдви. Гхязю ёънъгюъ вгъ едвдшбх еёдшёхввх югзиюзючгдшд зюех, жеълюхбсгд ждэщхггхф щбф юэимъгюф деёъщъбъггрк жздёдг мъбдчъмъжадя щиню. Въгф вдьгд гхэчхзс дзлдв Вхзёюлр, х ъы, цъэ еёъичъбюмъгюф, вхзъёсу.";
-        CaesarCipher caesarCipher = new CaesarCipher();
-        for (int i = 0; i < 34; i++) {
-            System.out.println(caesarCipher.getDecryptedTextUsingAlphabet(text, i));
+        String encryptedText = "Еъёчхф Вхзёюлх, адздёиу ф ждэщхб, црбх еёдюэчъщъгюъв южаижжзчх, " +
+                "ждчъёнъгжзчдв. Ъы зёюивй жёхчгюв бюнс ж ъы вдгивъгзхбсгрв аёхкдв. Зъеъёс " +
+                "вгъ дмъчющгд, мзд гъюэцъьгджзс тздшд аёхкх фчбфъзжф жбъщжзчюъв гъждчъёнъгжзчх " +
+                "мъбдчъмъжадшд югщючющиивх. Ф юэимюб чхни южздёюу ю чгыж юэвъгъгюф, здмгъъ" +
+                " дзёхэюч еджздфггиу юэвъгмючджзс мъбдчъмъжаюк едёдадч. Ю зъв гъ въгъъ, " +
+                "еджбъщдчхбх гъищхмх. Ф еёюнъб а чрчдщи, мзд чюгдя чжъви вдя югзъббъаз, х " +
+                "чдэвдьгд, вды мёъэвъёгдъ жзёъвбъгюъ ад чжъви шхёвдгюмгдви. Гхязю ёънъгюъ " +
+                "вгъ едвдшбх еёдшёхввх югзиюзючгдшд зюех, жеълюхбсгд ждэщхггхф щбф юэимъгюф " +
+                "деёъщъбъггрк жздёдг мъбдчъмъжадя щиню. Въгф вдьгд гхэчхзс дзлдв Вхзёюлр, х " +
+                "ъы, цъэ еёъичъбюмъгюф, вхзъёсу.";
+
+        for (int i = 0; i < 33; i++) {
+            System.out.println(new CaesarCipher().decryptText(encryptedText, Alphabets.RUSSIAN.getAlphabet(), i));
         }
     }
 
-
-    public String getDecryptedTextUsingAlphabet(String encryptedText, int shift) {
-        String alphabetString = "АаБбВвГгДдЕеЁёЖжЗзИиЙйКкЛлМмНнОоПпРрСсТтУуФфХхЦцЧчШшЩщЪъЫыЬьЭэЮюЯя";
-        char[] arrayOfCharsFromString = encryptedText.toCharArray();
+    public String decryptText(String encryptedText, String alphabet, int shift) {
+        final int SHIFT_MULTIPLIER = 2;
         StringBuilder decryptedText = new StringBuilder();
-        for (int i = 0; i < arrayOfCharsFromString.length; i++) {
-            if (!Character.isLetter(arrayOfCharsFromString[i])) {
-                decryptedText.append(arrayOfCharsFromString[i]);
+
+        shift = shift >= alphabet.length() / SHIFT_MULTIPLIER ? shift % (alphabet.length() / SHIFT_MULTIPLIER) : shift;
+
+        for (int i = 0; i < encryptedText.length(); i++) {
+            if (!Character.isLetter(encryptedText.charAt(i))) {
+                decryptedText.append(encryptedText.charAt(i));
                 continue;
             }
 
-            int letterIndexInAlphabet = alphabetString.indexOf(arrayOfCharsFromString[i]);
-            if (letterIndexInAlphabet + 2 * shift >= alphabetString.length()) {
-                decryptedText.append(alphabetString.charAt(2 * shift - (alphabetString.length() - letterIndexInAlphabet)));
+            int letterIndexInAlphabet = alphabet.indexOf(encryptedText.charAt(i));
+
+            if (letterIndexInAlphabet + SHIFT_MULTIPLIER * shift >= alphabet.length()) {
+                decryptedText.append(alphabet.charAt(SHIFT_MULTIPLIER * shift - (alphabet.length() - letterIndexInAlphabet)));
             } else {
-                decryptedText.append(alphabetString.charAt(letterIndexInAlphabet + 2 * shift));
+                decryptedText.append(alphabet.charAt(letterIndexInAlphabet + SHIFT_MULTIPLIER * shift));
             }
         }
 
         return decryptedText.toString();
     }
-
 }
